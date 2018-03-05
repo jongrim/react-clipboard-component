1db4087420e99170e2b2934afeedffb4a79c3131
Jonathan Grim <jgrim@pindrop.com>
2018-03-05 09:38:14 -0500

    add sourcemaps

--- rollup.config.js
+++ rollup.config.js
@@ -8,8 +8,8 @@ export default {
   input: 'src/clipboard.jsx',
   external: ['react', 'react-dom', 'prop-types'],
   output: [
-    { file: pkg.main, format: 'cjs' },
-    { file: pkg.module, format: 'es' }
+    { file: pkg.main, format: 'cjs', sourcemap: true },
+    { file: pkg.module, format: 'es', sourcemap: true }
   ],
   plugins: [resolve(), commonjs(), babel({ exclude: 'node_modules/**' })]
 };
