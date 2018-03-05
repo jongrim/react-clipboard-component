dbb532bc633605c8563387fa402fc628c5ad0c44
Jonathan Grim <jgrim@pindrop.com>
2018-03-04 23:45:23 -0500

    Add rollup and build process

---
+++ rollup.config.js
@@ -0,0 +1,15 @@
+import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+
+import pkg from './package.json';
+
+export default {
+  input: 'src/clipboard.jsx',
+  external: ['react', 'react-dom', 'prop-types'],
+  output: [
+    { file: pkg.main, format: 'cjs' },
+    { file: pkg.module, format: 'es' }
+  ],
+  plugins: [resolve(), commonjs(), babel({ exclude: 'node_modules/**' })]
+};
