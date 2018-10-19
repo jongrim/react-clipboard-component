4c6b8e9a4d78ac5f25f296901e6cf0408d600d0a
Jonathan Grim <jonjongrim@gmail.com>
2018-10-19 16:26:24 -0400

    add support for multidata and passing inner clipboard

--- src/clipboard.jsx
+++ src/clipboard.jsx
@@ -3,43 +3,69 @@ import PropTypes from 'prop-types';
 
 import clipboard from 'clipboard-polyfill';
 
-const Clipboard = ({ children, component, data, dataType, onCopy, render, text, ...other }) => {
+const Clipboard = ({
+  children,
+  component,
+  data,
+  dataType,
+  multidata,
+  onCopy,
+  render,
+  text,
+  ...other
+}) => {
   const callOnCopy = () => {
     if (onCopy && typeof onCopy === 'function') {
       return onCopy();
     }
   };
 
+  const innerClipboard = new clipboard.DT();
+
   const copyText = text => clipboard.writeText(text);
 
-  const copyData = (dataType, data) => {
-    const dt = new clipboard.DT();
-    dt.setData(dataType, data);
-    return clipboard.write(dt);
+  const copyData = () => {
+    return clipboard.write(innerClipboard);
   };
 
   const handleClick = () => {
+    if (!(data && dataType) && multidata.length === 0 && !text) {
+      throw new Error(
+        'No text prop or data and dataType prop specified. ' +
+          'You must provide the text, or data and dataType to be copied on click.'
+      );
+    }
+
     if (text) {
       return copyText(text).then(callOnCopy);
     }
 
-    if (data && dataType) {
-      return copyData(dataType, data).then(callOnCopy);
+    if (multidata.length > 0) {
+      multidata.forEach(i => {
+        setData(i.dataType, i.data);
+      });
     }
 
-    throw new Error(
-      'No text prop or data and dataType prop specified. ' +
-        'You must provide the text, or data and dataType to be copied on click.'
-    );
+    if (data && dataType) {
+      setData(dataType, data);
+    }
+
+    return copyData().then(callOnCopy);
+  };
+
+  const setData = (dataType, data) => {
+    innerClipboard.setData(dataType, data);
   };
 
   if (render) {
-    return render({ copyText, copyData, ...other });
+    return render({ clipboard: innerClipboard, copyText, copyData, ...other });
   }
 
   if (component) {
     const Component = component;
-    return <Component copyText={copyText} copyData={copyData} {...other} />;
+    return (
+      <Component clipboard={innerClipboard} copyText={copyText} copyData={copyData} {...other} />
+    );
   }
 
   return (
@@ -54,9 +80,19 @@ Clipboard.propTypes = {
   component: PropTypes.func,
   data: PropTypes.string,
   dataType: PropTypes.string,
+  multidata: PropTypes.arrayOf(
+    PropTypes.shape({
+      dataType: PropTypes.string,
+      data: PropTypes.string,
+    })
+  ),
   onCopy: PropTypes.func,
   render: PropTypes.func,
-  text: PropTypes.string
+  text: PropTypes.string,
+};
+
+Clipboard.defaultProps = {
+  multidata: [],
 };
 
 export default Clipboard;
